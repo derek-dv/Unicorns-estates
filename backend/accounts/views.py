961c65c73110c5d0e4f62408118b55452e0e9db1
Derek David <derekmiracledavid@hotmail.com>
2021-01-30 00:43:07 +0100

    accounts api created

---
+++ backend/accounts/views.py
@@ -0,0 +1,37 @@
+from django.contrib.auth import get_user_model
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import permissions
+
+User = get_user_model()
+
+
+class SignupView(APIView):
+    permission_classes = [
+        permissions.AllowAny,
+    ]
+
+    def post(self, request, format=None):
+        data = self.request.data
+
+        name = data["name"]
+        email = data["email"]
+        password = data["password"]
+        password2 = data["password2"]
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                return Response({"error": "Email already exists"})
+
+            else:
+                if len(password) < 6:
+                    return Response({"error": "Passwords must be at least 6 characters"})
+                else:
+                    user = User.objects.create_user(
+                        name=name, email=email, password=password)
+
+                    user.save()
+                    return Response({"success": "User created successfully"})
+
+        else:
+            return Response({"error": "Passwwords do not match"})
