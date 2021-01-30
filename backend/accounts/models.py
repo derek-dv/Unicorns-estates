d16afa3277718fb4fb228f2af638f17ecf88eaec
Derek David <derekmiracledavid@hotmail.com>
2021-01-30 11:23:20 +0100

    accounts app completed

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -4,13 +4,15 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseU
 
 class UserAccountManager(BaseUserManager):
     def create_user(self, email, name, password=None):
-        if not password:
-            raise ValueError("Users must have an email address")
+        if not email:
+            raise ValueError('Users must have an email address')
 
         email = self.normalize_email(email)
-        user = self.model(name=name, email=email)
+        user = self.model(email=email, name=name)
+
         user.set_password(password)
         user.save()
+
         return user
 
     def create_superuser(self, email, name, password):
@@ -18,6 +20,7 @@ class UserAccountManager(BaseUserManager):
 
         user.is_superuser = True
         user.is_staff = True
+        user.save()
 
         return user
 
@@ -30,10 +33,13 @@ class UserAccount(AbstractBaseUser, PermissionsMixin):
 
     objects = UserAccountManager()
 
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["name"]
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
+        return self.name
+
+    def get_short_name(self):
         return self.name
 
     def __str__(self):
