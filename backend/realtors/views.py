0ff65dbdd6eab2a253ec56cb51719f4724654b88
Derek David <derekmiracledavid@hotmail.com>
2021-01-30 14:48:18 +0100

    rea;tors app completed... Listings started

--- backend/realtors/views.py
+++ backend/realtors/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import permissions
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from .models import Realtor
+from .serializers import RealtorSerializer
 
-# Create your views here.
+
+class RealtorListView(ListAPIView):
+    permission_classes = [
+        permissions.AllowAny,
+    ]
+    queryset = Realtor.objects.all()
+    serializer_class = RealtorSerializer
+    pagination_class = None
+
+
+class RealtorView(RetrieveAPIView):
+    queryset = Realtor.objects.all()
+    serializer_class = RealtorSerializer
+
+
+class TopSellerView(ListAPIView):
+    permission_classes = [
+        permissions.AllowAny,
+    ]
+    queryset = Realtor.objects.filter(top_seller=True)
+    serializer_class = RealtorSerializer
+    pagination_class = None
--- backend/unicorn/settings.py
+++ backend/unicorn/settings.py
@@ -40,7 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
-    'accounts'
+    'accounts',
+    'realtors',
+    'listings',
 ]
 
 MIDDLEWARE = [
