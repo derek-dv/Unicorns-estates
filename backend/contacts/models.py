b0c14f47e6074efb11d1421e71ff107092c35033
Derek David <derekmiracledavid@hotmail.com>
2021-01-31 21:19:10 +0100

    contacts app created

--- backend/contacts/models.py
+++ backend/contacts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from datetime import datetime
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    subject = models.CharField(max_length=100)
+    message = models.TextField(blank=True)
+    contact_date = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.email
 
 # Create your models here.
